1002797538056e4b01ad25d6a4113aa83bc8f980
abanoub maged <abanoubmagedmaher@gmail.com>
2025-05-19 21:22:30 +0300

    up

---
+++ Innovation.Development.DAL/Common/Entities/BaseAuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innovation.Development.DAL.Common.Entities
+{
+    internal class BaseAuditableEntity<TKey> : BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+
+    }
+}
---
+++ Innovation.Development.DAL/Entities/Students/Student.cs
@@ -0,0 +1,18 @@
+﻿using Innovation.Development.DAL.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innovation.Development.DAL.Entities.Students
+{
+    internal class Student : BaseEntity<int>
+    {
+        public required string Name { get; set; }
+        public required DateOnly DateOfBirth { get; set; }
+        public required string Address  { get; set; }
+
+    }
+}
