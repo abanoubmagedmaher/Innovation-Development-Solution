4cf990d4ba539905d5a2dfbb503779f5ffa3cea6
abanoub maged <abanoubmagedmaher@gmail.com>
2025-05-20 20:22:47 +0300

    up

---
+++ Innovation Development/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Innovation.Development.BLL.Services.Students;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Innovation_Development.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
